d0957a127c62123be5a42387144b490dba289fb9
abhishekhardi <abhihardi@gmail.com>
2023-11-07 16:29:41 +0530

    Update BDDrun.bat

--- Runbat/BDDrun.bat
+++ Runbat/BDDrun.bat
@@ -1,2 +1,2 @@
-cd  C:\Users\ABHISHEK\AppData\Local\Programs\Python\Python311\Scripts
-robot C:\Users\ABHISHEK\PycharmProjects\RobotAutomation\TestCase\BDD.robot
+cd  C:\Users\ABHISHEK\PycharmProjects\RobotAutomation\
+robot TestCase\BDD.robot
