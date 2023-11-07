0849e4b8edd58ab5777a87542720843ef7c6399a
DESKTOP-16UQNCM\ABHISHEK <abhihardi@gmail.com>
2023-11-07 16:42:57 +0530

    1

--- Runbat/BDDrun.bat
+++ Runbat/BDDrun.bat
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 cd  C:\Users\ABHISHEK\PycharmProjects\RobotAutomation\
 robot TestCase\BDD.robot
+=======
+cd C:\Users\ABHISHEK\AppData\Local\Programs\Python\Python311\Scripts
+robot C:\Users\ABHISHEK\PycharmProjects\RobotAutomation\TestCase\BDD.robot
+>>>>>>> 641dc5f (bdd)
